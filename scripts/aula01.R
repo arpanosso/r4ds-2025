2a4ea799ae3cd0b300b0f31fb49c2513535564ac
Alan Rodrigo Panosso <arpanosso@yahoo.com.br>
2025-08-18 11:14:37 -0300

    aula 01

---
+++ scripts/aula01.R
@@ -0,0 +1,39 @@
+# Aula 01 de R4ds - 12/08/2025
+# Operações aritméticas
+2+2
+2*2
+5/2
+5%/%2 # divisão inteira
+5%%2 # resto da divisão
+sqrt(225)
+log10(1000)
+log2(1024)
+log(10) #log natural
+exp (2.302585) # exponencial
+pi
+round(pi,3) #definição de casas decimais
+2^10
+2**10
+# Operações relacionais
+2>5
+2<5
+5<= 25/5
+5  == 5 #igual
+5 != 5 #diferente
+
+#operações lógicas
+#conjunção E
+TRUE & TRUE
+TRUE & FALSE
+FALSE & TRUE
+FALSE & FALSE
+
+#disjunção OU
+TRUE | TRUE
+TRUE | FALSE
+FALSE | TRUE
+FALSE | FALSE
+
+#negação NÃO
+!TRUE
+!FALSE
